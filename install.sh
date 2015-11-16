e282945f0586b4e83b06a7fca7cc2580ba196dfa
matpierangeli <mat.pierangeli@gmail.com>
2015-11-17 00:05:11 +0100

    install vundle and vim airline plugin

--- install.sh
+++ install.sh
@@ -1,17 +1,30 @@
 #!/bin/zsh
 
+CONSOLE_PREFIX="\t->"
 cd "$(dirname "$0")"
 
 #shell
+echo "$CONSOLE_PREFIX Copy .zshrc..."
 rm -f ~/.zshrc
 cp -f zshrc ~/.zshrc
 
 #vim
+echo "$CONSOLE_PREFIX Install Solarized color scheme for Vim..."
 if [ ! -f ~/.vim/colors/solarized.vim ]; then #https://github.com/altercation/Vim-colors-solarized - Copyright (c) 2011 Ethan Schoonover
   rm -rf ~/.vim
   mkdir ~/.vim
   cp -rf vim/* ~/.vim/
 fi
 
+echo "$CONSOLE_PREFIX Copy .vimrc..."
 rm -f ~/.vimrc
 cp -f vimrc ~/.vimrc
+
+echo "$CONSOLE_PREFIX Install Vundle and Vim plugins..."
+if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
+  rm -rf ~/.vim/bundle
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim >/dev/null 2>&1
+  vim +PluginInstall +qall >/dev/null 2>&1
+fi
+
+echo "$CONSOLE_PREFIX Done."
--- vimrc
+++ vimrc
@@ -1,8 +1,21 @@
+"-> vundle
+set nocompatible
+filetype off
+set rtp+=~/.vim/bundle/Vundle.vim
+call vundle#begin()
+Plugin 'VundleVim/Vundle.vim'
+Plugin 'bling/vim-airline'
+call vundle#end()
+filetype plugin indent on
+
+"-> custom
+set laststatus=2
 set number
 set tabstop=4
 set expandtab
 syntax enable
 
-set background=dark
+"-> colors
 let g:solarized_termcolors=256
+set background=dark
 colorscheme solarized
